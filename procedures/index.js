b7bb505ea3b9ef44e5876e91ebfa74e7532f41b7
ting <tinglin0708@gmail.com>
2023-04-04 15:23:28 -0700

    age restriction

--- procedures/index.js
+++ procedures/index.js
@@ -42,7 +42,7 @@ var instructions = {
   stimulus: `
         <div class="text" id="instruction">
             <img src="../images/stanford_logo.png" alt="stanford logo" width="180" height="80">
-            <p><b>Only complete this study if you are a Singaporean citizen or Singapore Permanent Resident (PR).</b></p>
+            <p><b>Only complete this study if you are a Singaporean citizen or Singapore Permanent Resident (PR) over the age of 18.</b></p>
             <p>Please share this link with other Singaporean citizens and Singapore PRs, but do not participate in this study more than once.
             <BR>You will not be compensated more than once.</p> 
             <p>In this experiment, you will be listening to pairs of audio clips.</p>
@@ -206,7 +206,7 @@ var survey1 = {
         type: 'drop-down',
         prompt: "What year were you born?",
         name: 'age',
-        options: ['2002', '2001', '2000', '1999', '1998', '1997', '1996', '1995', '1994', '1993', '1992', '1991', '1990', '1989', '1988', '1987', '1986', '1985', '1984', '1983', '1982', '1981', '1980', '1979', '1978', '1977', '1976', '1975', '1974', '1973', '1972', '1971', '1970', '1969', '1968', '1967', '1966', '1965', '1964', '1963', '1962', '1961', '1960', '1959', '1958', '1957', '1956', '1955', '1954', '1953', '1952', '1951', '1950', '1949', '1948', '1947', '1946', '1945', '1944', '1943', '1942', '1941', '1940', '1939', '1938', '1937', '1936', '1935', '1934', '1933', 'Prefer not to answer'],
+        options: ['2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998', '1997', '1996', '1995', '1994', '1993', '1992', '1991', '1990', '1989', '1988', '1987', '1986', '1985', '1984', '1983', '1982', '1981', '1980', '1979', '1978', '1977', '1976', '1975', '1974', '1973', '1972', '1971', '1970', '1969', '1968', '1967', '1966', '1965', '1964', '1963', '1962', '1961', '1960', '1959', '1958', '1957', '1956', '1955', '1954', '1953', '1952', '1951', '1950', '1949', '1948', '1947', '1946', '1945', '1944', '1943', '1942', '1941', '1940', '1939', '1938', '1937', '1936', '1935', '1934', '1933', 'Prefer not to answer'],
         required: true,
       },
       {
